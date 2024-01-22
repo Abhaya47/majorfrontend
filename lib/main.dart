129071daeaaa031afa0de90cd6ff6ee45dfb6293
drishyaa1 <drishyabhattarai@gmail.com>
2024-01-22 17:46:16 +0545

    initial

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:majorproject/first.dart';
+import 'package:majorproject/second.dart';
+import 'package:majorproject/third.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Page',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/image/logo.png'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const MyLogin()),
+                );
+              },
+              child: Text('Login'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyRegistration()),
+                );
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
---
+++ lib/third.dart
@@ -0,0 +1,127 @@
+//home page
+import 'package:flutter/material.dart';
+
+class MyTabBar extends StatefulWidget {
+  const MyTabBar({Key? key}) : super(key: key);
+
+  @override
+  State<MyTabBar> createState() => _MyTabBarState();
+}
+
+class _MyTabBarState extends State<MyTabBar> {
+  int selectedIndex = 0;
+
+  final widgetOptions = [
+    // ChatFragment(),
+    // StatusFragment(),
+    // CallFragment(),
+
+    Home(),
+    Start(),
+    Log()
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  final widgetTitle = {"home", "start", "log"};
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: widgetOptions.elementAt(selectedIndex),
+      ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+        title: Text(
+          "NutriCoach",
+          style: TextStyle(fontStyle: FontStyle.italic),
+          textAlign: TextAlign.center,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.person),
+          )
+        ],
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.more_vert),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+        fixedColor: Colors.blue,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.house,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.start), label: 'Start'),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Log')
+        ],
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
+
+class Start extends StatefulWidget {
+  const Start({Key? key}) : super(key: key);
+
+  @override
+  State<Start> createState() => _Start();
+}
+
+class _Start extends State<Start> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Start'),
+      ),
+    );
+  }
+}
+
+class Log extends StatefulWidget {
+  const Log({Key? key}) : super(key: key);
+
+  @override
+  State<Log> createState() => _Log();
+}
+
+class _Log extends State<Log> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Log'),
+      ),
+    );
+  }
+}
